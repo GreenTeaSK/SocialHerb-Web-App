b8a3dfd92655187403fa93a53e1983bd59573e51
Whan <sweet.whankungz@hotmail.com>
2016-05-06 22:18:13 +0700

    Create DataTable Herb

--- SocialHerb/WebService.asmx.cs
+++ SocialHerb/WebService.asmx.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data;
+using System.Diagnostics;
 using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Text;
+using SocialHerb.Models;
+using SocialHerb;
+
 
 namespace SocialHerb
 {
@@ -14,13 +22,50 @@ namespace SocialHerb
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
+
     public class WebService : System.Web.Services.WebService
     {
-
-        [WebMethod]
-        public string HelloWorld()
+       
+                [WebMethod]
+        public DataTable Herb(string herbName)
         {
-            return "Hello World";
-        }
+            SqlConnection objConn = new SqlConnection();
+            SqlCommand objCmd = new SqlCommand();
+            SqlDataAdapter dtAdapter = new SqlDataAdapter();
+
+            DataSet ds = new DataSet();
+            DataTable dt = null;
+           // string strConnString = null;
+            StringBuilder strSQL = new StringBuilder();
+
+            /*function OpenConnection() as Boolean
+            try {
+
+                SqlConnection sqlConn = new SqlConnection("Data Source=(LocalDb)\v11.0;Initial Catalog=HerbDB;Persist Security Info=True;User ID=admin;Password=admin; Integrated Security=True; MultipleActiveResultSets=true;Pooling = false");
+                return true;
+              }
+            catch {
+                return false;
+               }*/
+
+
+
+            strSQL.Append(" SELECT * FROM Herb ");
+            strSQL.Append(" WHERE [herbName] = '" + herbName + "' ");
+            string SqlConnection = null;
+            objConn.ConnectionString = SqlConnection;
+            var _with1 = objCmd;
+            _with1.Connection = objConn;
+            _with1.CommandText = strSQL.ToString();
+            _with1.CommandType = CommandType.Text;
+            dtAdapter.SelectCommand = objCmd;
+            dtAdapter.Fill(ds);
+            dt = ds.Tables[0];
+            dtAdapter = null;
+            objConn.Close();
+            objConn = null;
+
+            return dt;
+            }
     }
 }
