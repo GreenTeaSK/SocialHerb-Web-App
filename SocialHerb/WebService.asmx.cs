a884b67bc4edcfc0872e0d69c108accb7cf22d80
Whan <sweet.whankungz@hotmail.com>
2016-05-07 13:15:07 +0700

    Edit DataTable Herb

--- SocialHerb/WebService.asmx.cs
+++ SocialHerb/WebService.asmx.cs
@@ -25,47 +25,88 @@ namespace SocialHerb
 
     public class WebService : System.Web.Services.WebService
     {
-       
-                [WebMethod]
-        public DataTable Herb(string herbName)
+
+        [WebMethod]
+
+        public string Herb(string herbName)
         {
-            SqlConnection objConn = new SqlConnection();
-            SqlCommand objCmd = new SqlCommand();
-            SqlDataAdapter dtAdapter = new SqlDataAdapter();
-
-            DataSet ds = new DataSet();
-            DataTable dt = null;
-           // string strConnString = null;
-            StringBuilder strSQL = new StringBuilder();
-
-            /*function OpenConnection() as Boolean
-            try {
-
-                SqlConnection sqlConn = new SqlConnection("Data Source=(LocalDb)\v11.0;Initial Catalog=HerbDB;Persist Security Info=True;User ID=admin;Password=admin; Integrated Security=True; MultipleActiveResultSets=true;Pooling = false");
-                return true;
-              }
-            catch {
-                return false;
-               }*/
-
-
-
-            strSQL.Append(" SELECT * FROM Herb ");
-            strSQL.Append(" WHERE [herbName] = '" + herbName + "' ");
-            string SqlConnection = null;
-            objConn.ConnectionString = SqlConnection;
-            var _with1 = objCmd;
-            _with1.Connection = objConn;
-            _with1.CommandText = strSQL.ToString();
-            _with1.CommandType = CommandType.Text;
-            dtAdapter.SelectCommand = objCmd;
-            dtAdapter.Fill(ds);
-            dt = ds.Tables[0];
-            dtAdapter = null;
-            objConn.Close();
-            objConn = null;
-
-            return dt;
+           
+            SqlConnection con = new SqlConnection("Data Source=(LocalDb)\v11.0;Initial Catalog=HerbDB;Persist Security Info=True;User ID=admin;Password=admin; Integrated Security=True; MultipleActiveResultSets=true");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Herb ", con);
+            con.Open();
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
             }
+            catch (Exception e)
+            {
+                con.Close();
+                return "Failed";
+
+            }
+            SqlDataReader HerbDBContext = cmd.ExecuteReader();
+            using (HerbDBContext)
+            {
+                while (HerbDBContext.Read())
+                {
+                 var   result = HerbDBContext[0].ToString();
+
+
+
+                }
+                HerbDBContext.Close();
+                con.Close();
+
+                return null;
+
+            }
+
+            /* 
+               {
+
+                   SqlConnection objConn = new SqlConnection();
+                   SqlCommand objCmd = new SqlCommand();
+                   SqlDataAdapter dtAdapter = new SqlDataAdapter();
+
+                   DataSet ds = new DataSet();
+                   DataTable dt = null;
+                  // string strConnString = null;
+                   StringBuilder strSQL = new StringBuilder();
+
+                  //function OpenConnection() as Boolean
+                  try {
+
+
+                       SqlConnection sqlConn = new SqlConnection("Data Source=(LocalDb)\v11.0;Initial Catalog=HerbDB;Persist Security Info=True;User ID=admin;Password=admin; Integrated Security=True; MultipleActiveResultSets=true");
+
+
+                   }
+                 catch(System.InvalidOperationException) {
+
+                     }
+
+
+
+                   strSQL.Append(" SELECT * FROM Herb ");
+                   strSQL.Append(" WHERE [herbName] = '" + herbName + "' ");
+                   string SqlConnection = null;
+                   objConn.ConnectionString = SqlConnection;
+                   var _with1 = objCmd;
+                   _with1.Connection = objConn;
+                   _with1.CommandText = strSQL.ToString();
+                   _with1.CommandType = CommandType.Text;
+               dtAdapter.SelectCommand = objCmd;
+                  dtAdapter.Fill(ds);
+
+                  dt = ds.Tables[0];
+                 dtAdapter = null;
+                   objConn.Close();
+                   objConn = null;
+
+                   return dt;
+                   }
+           }*/
+        }
     }
 }
